76658ece61fafa3689faefdb0c96b3e0f11af4b9
rayalva407 <rayalva407@gmail.com>
2021-01-15 17:46:47 -0700

    adds more transition effects

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ window.addEventListener('keydown', function(e) {
 
   const audio = document.querySelector(`audio[data-key="${e.key}"]`)
   const key = document.querySelector(`.key[data-key="${e.key}"]`)
-  
+
   if (!audio) return
   audio.currentTime = 0
   audio.play()
   key.classList.add('playing');
+  setTimeout(function() {key.classList.remove('playing')}, 200)
 })
