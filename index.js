325cc275cdd98ef81eb324e6a7a70cb50ff5f514
rayalva407 <rayalva407@gmail.com>
2021-01-15 17:35:31 -0700

    adds change transition for keys

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 window.addEventListener('keydown', function(e) {
 
   const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+  const key = document.querySelector(`.key[data-key="${e.key}"]`)
+  
   if (!audio) return
   audio.currentTime = 0
   audio.play()
+  key.classList.add('playing');
 })
